8d0a202174df78290d5643e6c6aa41ff5ee29392
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-03 15:23:28 +0530

    JS Arithmetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,17 @@
+//Arithmetic operators
+console.log("arithmetic Operators");
+const a=2;
+const b=3;
+const c=a+b;
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);//modulus
+console.log("a^b = ",a);
+console.log("a**b = ",a**b);
+// console.log("a++ = ",a++);//post-increment
+// console.log("++a = ",++a);//pre-increment
+// console.log("a-- = ",a--);//post-decrement
+// console.log("--a = ",--a);//pre-decrement
+console.log(c);
