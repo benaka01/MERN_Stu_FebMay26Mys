2f1c66b85bb05ad58634e5d42641fb6f82fba1c3
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-03 15:37:08 +0530

    JS : Function expression

---
+++ W3/D2/P6.js
@@ -0,0 +1,6 @@
+//Function expression
+const greet  = function greet(user){// Function declaration
+console.log("Hello ," ,user+ "!");//function definition
+}
+greet("Benaka");//function call
+greet("Varsh");
