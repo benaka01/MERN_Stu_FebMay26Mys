30260a0e797b677659e6885c3507e2641f76976c
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-03 15:24:14 +0530

    JS Comparison Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+// Comparison Operators
+console.log("Comparison Operators");
+let x=2,y="2";
+console.log("x==y",x==y);//Loose Equality
+console.log("x===y",x===y);//Strict Equality
+console.log("x!=y",x!=y);//Loose Inequality
+console.log("x!==y",x!==y);//Strict Inequality
+console.log("x>3",x>3);//greater than 3
+console.log("x<3",x<3);//Lesser than 3
+console.log("x>=3",x>=3);//greater than or equals 3
+console.log("x<=3",x<=3);//Lesser than or equals 3
