3db9ad60700d73f7756926713b584d7809a08551
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-09 20:34:29 +0530

    Basics of Click events Using JS

---
+++ W3/D5/P1.js
@@ -0,0 +1,11 @@
+//basics of click me
+ const button =document.getElementById("clickbtn");
+
+ button.addEventListener("click", function(){
+    console.log("Button is clicked");
+    
+ });
+  button.addEventListener("click", function(){
+    console.log("Second Event Listener : Button is clicked");
+    
+ });
