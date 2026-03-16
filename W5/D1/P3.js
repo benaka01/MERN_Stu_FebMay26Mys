c0300acbe3e99a5545d70227ae0ae88c6a1f1416
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-16 16:29:04 +0530

    Using SetTimeout function to delay the output

---
+++ W5/D1/P3.js
@@ -0,0 +1,8 @@
+console.log("Node.js architecture Demo");
+console.log("1.Script started ");
+
+//setTimeOut
+setTimeout(()=>{
+    console.log("3.Timer callback finished after waiting");
+},3000);
+console.log("2.Script continued without waiting for timer callback");
