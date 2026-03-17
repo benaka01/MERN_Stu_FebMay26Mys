daea8c5fbf7d8be86123549677d18758c69a3701
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-17 13:31:55 +0530

    Callback Error

---
+++ W5/D2/P5.js
@@ -0,0 +1,24 @@
+//Callback Error
+function divideNumbers(a,b,callback) {
+    if(b==0){
+        callback("Cannot divide by zero.",null);
+        return;
+    }
+    const result=a/b;
+    callback(null,result);
+}
+divideNumbers(10,2,function(error,result){
+    if(error){
+        console.log("Error :"+error);
+        return;
+    }
+    console.log("Result : ",result);
+});
+
+divideNumbers(10,0,function(error,result){
+    if(error){
+        console.log("Error :"+error);
+        return;
+    }
+    console.log("Result : ",result);
+});
