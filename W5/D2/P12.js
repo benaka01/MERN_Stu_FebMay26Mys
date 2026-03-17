c9a995eb9ad24c16126523b03ac862711b1835a9
benaka01 <benaka.vishwakarma@gmail.com>
2026-03-17 16:20:27 +0530

    Chaining promises with returned promises

---
+++ W5/D2/P12.js
@@ -0,0 +1,34 @@
+//  Chaining promises with centraized error handling
+
+function validateLogin(){
+    return new Promise((resolve) => {
+        setTimeout(function(){
+            resolve("Login validated");
+        },1000);
+    });
+}
+function fetchAccountData() {
+    return new Promise((resolve,reject) => {
+        setTimeout(function(){
+            const accountFound=true;
+            if(accountFound) {
+                resolve("Account data loaded");
+            }
+            else{
+                reject("Account Data could not found.");
+            }
+        },800);
+    });
+}
+
+validateLogin()
+.then(function(message){
+    console.log(message);
+    return fetchAccountData();
+})
+.then(function(accountMessage){
+    console.log(accountMessage);
+})
+.catch(function(error){
+    console.log("Chain Error ",error);
+});
